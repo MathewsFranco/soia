1860d54f9eb4bde7d2be9ca02117910914352726
Mathews Franco <mathewsandre.franco@gmail.com>
2025-10-07 19:52:17 +0200

    🔧 update settings to use GH pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,12 @@
-import { defineConfig } from 'vite'
-import viteReact from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
-
 import { TanStackRouterVite } from '@tanstack/router-plugin/vite'
+import { defineConfig } from 'vite'
 import { resolve } from 'node:path'
+import tailwindcss from '@tailwindcss/vite'
+import viteReact from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/soia/',
   plugins: [
     TanStackRouterVite({ autoCodeSplitting: true }),
     viteReact(),
