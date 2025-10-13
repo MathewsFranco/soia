fd8d3fb02af093d1f9410800bcc3f43f426fb910
Mathews Franco <mathewsandre.franco@gmail.com>
2025-10-13 15:13:47 +0200

    🚧 add footer component

---
+++ src/components/Footer.tsx
@@ -0,0 +1,11 @@
+const Footer = () => {
+  return (
+    <div className="bg-black text-white ">
+      <img src="/soia-logo-white.png" alt="soia-logo" />
+      <p className="p-4">Follow us</p>
+      <p className="p-4">soiaconnectcomercial@gmail.com</p>
+    </div>
+  )
+}
+
+export default Footer
