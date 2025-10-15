8ebf0b876af79348d7ec4909de0fd122695059b8
Mathews Franco <mathewsandre.franco@gmail.com>
2025-10-15 17:21:46 +0200

    ✨ Fix about section

---
+++ src/components/About.tsx
@@ -0,0 +1,31 @@
+export function About() {
+  return (
+    <section className="bg-black px-20 py-25 gap-15 flex flex-col md:flex-row tracking-wide leading-9 text-2xl">
+      <div className="relative flex flex-col justify-center bg-black text-white w-full md:w-1/2">
+        <h1 className="font-bold mb-10 text-6xl max-w-xs">Sobre a SOIA</h1>
+        <div className="px-6 py-3">
+          {/* Decorative borders */}
+          <div className="absolute top-42 left-0 w-8 h-8 border-t-2 border-l-2 border-white" />
+          <div className="absolute bottom-4 right-4 w-8 h-8 border-b-2 border-r-2 border-white" />
+          <p>
+            Mais do que comunicar, buscamos entender o que move o consumo e
+            traduzir essas percepções em posicionamentos sólidos, narrativas
+            potentes e experiências marcantes.
+          </p>
+          <p>
+            Nosso trabalho une análise sociocultural, branding, inovação e
+            storytelling para criar marcas que transcendem produtos e se tornam
+            parte da vida das pessoas.
+          </p>
+        </div>
+      </div>
+      <div className="aspect-[5/4] w-full md:w-1/2 flex items-center justify-center">
+        <img
+          src="/magazines.jpeg"
+          alt="Soia Logo"
+          className="h-full w-full object-cover object-[center_25%]"
+        />
+      </div>
+    </section>
+  )
+}
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,3 +1,4 @@
+import { About } from '../components/About'
 import { BrandDefinition } from '../components/BrandDefinition'
 import Footer from '@/components/Footer'
 import { createFileRoute } from '@tanstack/react-router'
@@ -15,25 +16,7 @@ function App() {
         className="h-160 w-full object-cover object-[center_70%]"
       />
       <BrandDefinition />
-      <div className=" bg-black text-white px-18 py-25 text-3xl gap-6 flex items-center justify-center flex-col sm:flex-row ">
-        <div className="flex-col justify-center align-middle bg-black text-white">
-          <h1 className="font-bold mb-10 text-6xl ">Sobre a Soia</h1>
-          <p className="text-2xl">
-            Mais do que comunicar, buscamos entender o que move o consumo e
-            traduzir essas percepções em posicionamentos sólidos, narrativas
-            potentes e experiências marcantes.
-            <br />
-            Nosso trabalho une análise sociocultural, branding, inovação e
-            storytelling para criar marcas que transcendem produtos e se tornam
-            parte da vida das pessoas.
-          </p>
-        </div>
-        <img
-          src="/magazines.jpeg"
-          alt="Soia Logo"
-          className="h-140 object-cover"
-        />
-      </div>
+      <About />
       <div className=" p-2 text-3xl text-ink my-8 gap-6 flex items-center justify-center flex-col sm:flex-row ">
         <h2 className="font-bold mb-4">Nossos Serviços</h2>
         <div>
