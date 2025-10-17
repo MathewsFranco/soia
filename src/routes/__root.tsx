1d58dee6633ea5af70c56289fd5a6edc92945adc
Mathews Franco <mathewsandre.franco@gmail.com>
2025-10-17 12:05:16 +0200

    🔥 remove unnecessary route and router dev tools

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,25 +1,12 @@
 import { Outlet, createRootRoute } from '@tanstack/react-router'
 
 import Header from '../components/Header'
-import { TanStackDevtools } from '@tanstack/react-devtools'
-import { TanStackRouterDevtoolsPanel } from '@tanstack/react-router-devtools'
 
 export const Route = createRootRoute({
   component: () => (
     <div className="min-h-screen">
       <Header />
       <Outlet />
-      <TanStackDevtools
-        config={{
-          position: 'bottom-right',
-        }}
-        plugins={[
-          {
-            name: 'Tanstack Router',
-            render: <TanStackRouterDevtoolsPanel />,
-          },
-        ]}
-      />
     </div>
   ),
 })
