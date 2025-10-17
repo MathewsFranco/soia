9238092c4b6b9f97fd7bac05810597f48af2a548
Mathews Franco <mathewsandre.franco@gmail.com>
2025-10-17 10:16:08 +0200

    ✨ fix founder component

---
+++ src/components/Founder.tsx
@@ -0,0 +1,32 @@
+export function Founder() {
+  return (
+    <div className="text-2xl py-20 px-35 text-white bg-sage flex flex-col items-center justify-center gap-16">
+      <h1 className="font-bold text-6xl mb-4 text-center">
+        Conheça nossa fundadora
+      </h1>
+
+      <div className="flex flex-col lg:flex-row items-center gap-20 ">
+        <div className="flex flex-col  items-center gap-10 ">
+          <div className="w-80 h-80 rounded-full overflow-hidden shadow-xl relative">
+            <img
+              src="/owner.jpeg"
+              alt="Soia Logo"
+              className="absolute inset-0 w-full h-full object-cover scale-160 translate-y-[27%] object-[295%_50%]"
+            />
+          </div>
+          <p>FABIANA TOMAZ</p>
+        </div>
+
+        <p className="max-w-xl leading-relaxed text-left">
+          Fabiana Tomaz é estrategista criativa e fundadora da SOIA. Com uma
+          trajetória marcada pela moda, branding e comportamento, transforma
+          cultura e tendências em estratégias que conectam marcas a pessoas de
+          forma genuína e relevante. Sua visão une sensibilidade estética,
+          pensamento estratégico e inovação para criar experiências que
+          transcendem o óbvio e constroem valor simbólico, emocional e
+          duradouro.
+        </p>
+      </div>
+    </div>
+  )
+}
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,6 +1,7 @@
 import { About } from '../components/About'
 import { BrandDefinition } from '../components/BrandDefinition'
 import Footer from '@/components/Footer'
+import { Founder } from '../components/Founder'
 import { Services } from '../components/Services'
 import { createFileRoute } from '@tanstack/react-router'
 
@@ -19,21 +20,7 @@ function App() {
       <BrandDefinition />
       <About />
       <Services />
-      <div className=" p-2 text-3xl text-ink my-8 gap-6 flex items-center justify-center flex-col sm:flex-row ">
-        <h2 className="font-bold mb-4">Conheça nossa fundadora</h2>
-        <div>
-          <p>
-            Fabiana Tomaz é estrategista criativa e fundadora da SOIA. Com uma
-            trajetória marcada pela moda, branding e comportamento, transforma
-            cultura e tendências em estratégias que conectam marcas a pessoas de
-            forma genuína e relevante. Sua visão une sensibilidade estética,
-            pensamento estratégico e inovação para criar experiências que
-            transcendem o óbvio e constroem valor simbólico, emocional e
-            duradouro.
-          </p>
-        </div>
-        <img src="/owner.jpeg" alt="Soia Logo" className="h-120" />
-      </div>
+      <Founder />
       <Footer />
     </>
   )
