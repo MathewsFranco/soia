5baefc0d70eb99dfc46026a214eb10208fe87621
Mathews Franco <mathewsandre.franco@gmail.com>
2025-10-21 10:38:17 +0200

    🔧 remove unnecessary routes

--- src/routeTree.gen.ts
+++ src/routeTree.gen.ts
@@ -9,14 +9,8 @@
 // Additionally, you should also exclude this file from your linter and/or formatter to prevent it from being checked or modified.
 
 import { Route as rootRouteImport } from './routes/__root'
-import { Route as SoiaRouteImport } from './routes/soia'
 import { Route as IndexRouteImport } from './routes/index'
 
-const SoiaRoute = SoiaRouteImport.update({
-  id: '/soia',
-  path: '/soia',
-  getParentRoute: () => rootRouteImport,
-} as any)
 const IndexRoute = IndexRouteImport.update({
   id: '/',
   path: '/',
@@ -25,39 +19,28 @@ const IndexRoute = IndexRouteImport.update({
 
 export interface FileRoutesByFullPath {
   '/': typeof IndexRoute
-  '/soia': typeof SoiaRoute
 }
 export interface FileRoutesByTo {
   '/': typeof IndexRoute
-  '/soia': typeof SoiaRoute
 }
 export interface FileRoutesById {
   __root__: typeof rootRouteImport
   '/': typeof IndexRoute
-  '/soia': typeof SoiaRoute
 }
 export interface FileRouteTypes {
   fileRoutesByFullPath: FileRoutesByFullPath
-  fullPaths: '/' | '/soia'
+  fullPaths: '/'
   fileRoutesByTo: FileRoutesByTo
-  to: '/' | '/soia'
-  id: '__root__' | '/' | '/soia'
+  to: '/'
+  id: '__root__' | '/'
   fileRoutesById: FileRoutesById
 }
 export interface RootRouteChildren {
   IndexRoute: typeof IndexRoute
-  SoiaRoute: typeof SoiaRoute
 }
 
 declare module '@tanstack/react-router' {
   interface FileRoutesByPath {
-    '/soia': {
-      id: '/soia'
-      path: '/soia'
-      fullPath: '/soia'
-      preLoaderRoute: typeof SoiaRouteImport
-      parentRoute: typeof rootRouteImport
-    }
     '/': {
       id: '/'
       path: '/'
@@ -70,7 +53,6 @@ declare module '@tanstack/react-router' {
 
 const rootRouteChildren: RootRouteChildren = {
   IndexRoute: IndexRoute,
-  SoiaRoute: SoiaRoute,
 }
 export const routeTree = rootRouteImport
   ._addFileChildren(rootRouteChildren)
