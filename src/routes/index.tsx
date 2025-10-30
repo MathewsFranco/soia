9f5077d8bc2928660d97ba8d149d911415e0bf66
Mathews Franco <mathewsandre.franco@gmail.com>
2025-10-30 10:09:32 +0100

    🔥 remove section

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,11 +1,10 @@
+import Footer from '@/components/Footer'
+import { createFileRoute } from '@tanstack/react-router'
 import { About } from '../components/About'
 import { BrandDefinition } from '../components/BrandDefinition'
-import Footer from '@/components/Footer'
 import { Founder } from '../components/Founder'
-import { ImageSpacer } from '../components/ImageSpacer'
 import { Services } from '../components/Services'
 import { Slogan } from '../components/Slogan'
-import { createFileRoute } from '@tanstack/react-router'
 
 export const Route = createFileRoute('/')({
   component: App,
@@ -15,7 +14,6 @@ function App() {
   return (
     <>
       <Slogan />
-      <ImageSpacer />
       <BrandDefinition />
       <About />
       <Services />
