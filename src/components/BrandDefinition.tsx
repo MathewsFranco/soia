2833b161a8be5fd066f7c420eddc72051007540e
Mathews Franco <mathewsandre.franco@gmail.com>
2025-10-30 10:47:05 +0100

    🔧 fix spacing and img positioning

--- src/components/BrandDefinition.tsx
+++ src/components/BrandDefinition.tsx
@@ -1,23 +1,23 @@
 export function BrandDefinition() {
   return (
     <section
-      className=" flex flex-col pt-10 md:pt-0 md:flex-row justify-center items-center bg-amber text-white "
+      className="flex flex-col pt-10 md:pt-0 md:flex-row justify-center items-center bg-amber text-white "
       id="brand-definition"
     >
-      <div className=" px-10 flex flex-col gap-10 items-start justify-center md:w-4/6">
+      <div className="mx-5  flex flex-col gap-10 items-start justify-center md:w-4/6  ">
         <h1 className="font-bold text-6xl ">QUEM SOMOS</h1>
         <p className="text-2xl text-justify">
           A SOIA é agência que conecta a sua marca as gerações que estão
           impulsionando o mercado.
         </p>
       </div>
-      <div className="p-15 m-5 ml-0  w-full md:w-3/6 flex relative items-center justify-center overflow-hidden">
-        <Square className="absolute top-0 left-0 m-5" />
-        <Square className="absolute bottom-0 right-0 m-5" />
+      <div className="max-w-100 my-15 md:w-2/6 md:mr-15 md:mb-15 relative ">
+        <Square className="absolute -top-8 -left-15" />
+        <Square className="absolute -bottom-10 -right-10 " />
         <img
           src="/model.jpeg"
           alt="model holding a computer"
-          className=" object-cover  object-[center_160%]"
+          className="  object-cover"
         />
       </div>
     </section>
@@ -25,5 +25,5 @@ export function BrandDefinition() {
 }
 
 const Square = ({ className }: { className?: string }) => (
-  <div className={`w-25 h-25 bg-sage ${className}`} />
+  <div className={`w-20 h-20 bg-sage ${className}`} />
 )
