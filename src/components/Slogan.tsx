46fce331efdd234867cacfe84423888d61d0b437
Mathews Franco <mathewsandre.franco@gmail.com>
2025-11-05 11:24:08 +0100

    🔧 improve text spacing

--- src/components/Slogan.tsx
+++ src/components/Slogan.tsx
@@ -3,10 +3,10 @@ export function Slogan() {
     <div className="bg-amber p-10 md:px-15 md:py-30 flex gap-8 items-center">
       <img
         src="/logomark-white.png"
-        className="w-30 h-30 md:w-50 md:h-50 md:ml-10"
+        className="w-25 h-25 md:w-50 md:h-50 md:ml-10"
         alt="Descrição da imagem"
       />
-      <p className=" text-white text-2xl md:text-3xl text-justify leading-relaxed">
+      <p className="text-white text-xl md:text-3xl text-justify ">
         Mais do que comunicar, buscamos entender o que realmente move o consumo
         e transformamos essas percepções em posicionamentos sólidos, histórias
         autênticas e experiências memoráveis.
