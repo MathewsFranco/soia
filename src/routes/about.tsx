196d39b0db604626458ad6f664adf7a282f79dc6
Mathews Franco <mathewsandre.franco@gmail.com>
2025-12-12 12:42:32 +0100

    Refactor About page text and styles
    Updates founder's description and adjusts heading sizes on the About
    page. Enables hyphenation for better text flow on smaller screens.

--- src/routes/about.tsx
+++ src/routes/about.tsx
@@ -9,7 +9,7 @@ function RouteComponent() {
   return (
     <>
       <div className="bg-clay text-white flex items-center gap-10 px-5 md:px-15 text-justify py-30 flex-col md:flex-row">
-        <h1 className="font-cy-grotesk text-5xl  md:w-1/2 w-auto uppercase border-b-white border-b-3 text-center">
+        <h1 className="font-cy-grotesk text-4xl md:w-1/2 w-auto uppercase border-b-white border-b-3 text-center">
           quem somos
         </h1>
         <div className="text-2xl w-full md:w-1/2">
@@ -37,26 +37,11 @@ function RouteComponent() {
         </div>
       </div>
       <div className=" flex gap-10 px-5 md:px-15 text-justify py-30 flex-col">
-        <h1 className="font-cy-grotesk text-4xl md:text-5xl w-auto self-start border-b-black border-b-3 text-center uppercase">
+        <h1 className="font-cy-grotesk text-2xl md:text-5xl w-auto self-start border-b-black border-b-3 text-center uppercase">
           além da superfície
         </h1>
         <p className="text-2xl md:ml-55">
-          Fabiana Tomaz é estrategista criativa e fundadora da SOIA, busca
-          construir sua trajetória entre a moda, o branding e o comportamento de
-          consumo. Na SOIA, cada projeto vai além de uma simples entrega: é uma
-          verdadeira experiência de criação. Mergulhamos no universo da marca,
-          estudamos tendências, analisamos comportamentos e entendemos o que
-          realmente motiva e emociona as pessoas. A partir disso, desenvolvemos
-          narrativas e soluções que dão vida à marca, fortalecendo sua presença
-          e relevância. Nosso objetivo é claro: ajudar marcas a alavancar seus
-          negócios e se conectar de forma autêntica com as gerações que moldam o
-          mercado atual. Transformamos ideias, sentimentos e insights em
-          experiências que carregam significado e deixam uma impressão
-          duradoura. Na SOIA, estratégia e sensibilidade caminham lado a lado.
-          Cada ação, cada história e cada detalhe são pensados para criar
-          conexões reais, memoráveis e profundas entre marcas e pessoas
-          construindo relações que vão além do consumo, tornando a marca parte
-          da vida e da experiência de quem a encontra.
+          Fabiana Tomaz é estrategista criativa e fundadora da SOIA. Formada em Moda, ela constrói sua trajetória unindo a moda, o branding e o comportamento de consumo. Na SOIA, cada projeto vai além de uma simples entrega: é uma verdadeira experiência de criação. Mergulhamos no universo da marca, estudamos tendências, analisamos comportamentos e entendemos o que realmente motiva e emociona as pessoas. A partir disso, desenvolvemos narrativas e soluções que dão vida à marca, fortalecendo sua presença e relevância no mercado.
         </p>
       </div>
       <Footer color="sage" />
